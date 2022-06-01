f405e958587ce050dfb7f1ab3c32702c1f9b9a99
poornesh <poorneshchenna29@gmail.com>
2022-06-01 23:33:04 +0530

    added payment route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
-const stripe = require('stripe')('')
+const { nanoid } = require('nanoid')
+const stripe = require('stripe')(process.env.stripe_SECRETKEY)
 
 const app = express()
 app.use(express.json())
@@ -8,6 +9,31 @@ app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('server is up and running')
+})
+
+app.post('/payment', (req, res) => {
+  const { product, token } = req.body
+  const idempotencyKey = nanoid()
+
+  return stripe.customers
+    .create({
+      email: token.email,
+      source: token.id,
+    })
+    .then((customer) => {
+      stripe.charges.create(
+        {
+          amount: product.price,
+          currency: 'INR',
+        },
+        { idempotencyKey }
+      )
+    })
+    .then((result) => res.status(200).json(result))
+    .catch((err) => {
+      console.log(err)
+      return res.status(500).send('internal server error')
+    })
 })
 
 app.listen(5000, () => {
